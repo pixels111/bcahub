afd2ef82deb5972947f24cf4f93c013c593a388c
Sethu182006 <sethuthangella2006@gmail.com>
2025-05-06 15:53:06 +0530

    add

--- resources.js
+++ resources.js
@@ -89,7 +89,7 @@ document.addEventListener('DOMContentLoaded', () => {
     },
     3: {
       assignments: [
-        { title: "DBMS assignment(by SETHU)", link:"fast-double-click-on-mouse.mp3"},
+        { title: "DBMS assignment(by SETHU)", link:"DBMS assignment (by SETHU).pdf"},
         { title: "Assignment 2", link: "#" }
       ],
       notes: Array.from({ length: 7 }, (_, i) => ({
