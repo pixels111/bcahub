005a0e6dff1f1bcb685437c308393b4fa89200fa
Sethu182006 <sethuthangella2006@gmail.com>
2025-07-18 20:06:08 +0530

    5th sem minor sub update

--- resources/index652536979.js
+++ resources/index652536979.js
@@ -243,6 +243,9 @@ document.addEventListener('DOMContentLoaded', function() {
            5:{
                 "major": [
                 { name: "4th sem major subjects syllabus(edited)", link: "https://drive.google.com/uc?export=download&id=1JwvSkophXiZ7WPr9Fj-mMDCvyaJJRGn9" },
+                ],
+                "minor": [
+                { name: "4th sem minor subjects syllabus(edited)", link: "https://drive.google.com/uc?export=download&id=1DFxpdGZd_Uq7r7B03ne6dbdYhy912FmL" },
 
             ],
 
